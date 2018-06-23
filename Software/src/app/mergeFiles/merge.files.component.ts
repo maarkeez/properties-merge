904c91c0ecc0d66619669b34f3702eda87d5e834
David Márquez Delgado <d.marquez.delgado.91@gmail.com>
2018-06-23 17:44:34 +0200

    [1.0.0] Merge GUI

--- Software/src/app/mergeFiles/merge.files.component.ts
+++ Software/src/app/mergeFiles/merge.files.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { NgForm } from '@angular/forms';
 import { Property } from "./property.model";
+import { PropertyDifference } from "./property.difference.model";
 
 
 @Component( {
@@ -12,8 +13,9 @@ import { Property } from "./property.model";
 export class MergeFilesComponent {
 
     // MARK: - Local Variables
-    leftSideProperties: Property[] = [];
-    rightSideProperties: Property[] = [];
+    leftSideMap: Map<string, Property> = new Map<string, Property>();
+    rightSideMap: Map<string, Property> = new Map<string, Property>();
+    differencesMap: Map<string, PropertyDifference> = new Map<string, PropertyDifference>();
 
     // MARK: - IBActions
 
@@ -21,16 +23,63 @@ export class MergeFilesComponent {
 
     // MARK: - Utils
     loadLeftSideProperties() {
-        for ( let i = 0; i < 5; i++ ) {
-            var property = new Property( i, "property_" + i, "value_" + i );
-            this.leftSideProperties[i] = property;
+        let firstp = new Property( 0, "property_0", "value_Diferent" );
+        this.leftSideMap.set( firstp.name, firstp );
+
+        for ( let i = 1; i < 5; i++ ) {
+            let property = new Property( i, "property_" + i, "value_" + i );
+            this.leftSideMap.set( property.name, property );
+
+        }
+
+        for ( let i = 5; i < 9; i++ ) {
+            let property = new Property( i, "property_" + ( i + 4 ), "value_" + ( i + 4 ) );
+            this.leftSideMap.set( property.name, property );
+        }
+        this.checkDifferences();
+    }
+
+    loadRightSideProperties() {
+        for ( let i = 0; i < 9; i++ ) {
+            let property = new Property( i, "property_" + i, "value_" + i );
+            this.rightSideMap.set( property.name, property );
+        }
+        this.checkDifferences();
+    }
+
+    checkDifferences() {
+        this.differencesMap = new Map();
+
+        for ( let key of Array.from( this.leftSideMap.keys() ) ) {
+            let propertyDifference = new PropertyDifference( key, this.leftSideMap.get( key ), this.rightSideMap.get( key ) );
+            this.differencesMap.set( key, propertyDifference );
+            if(propertyDifference.equal || propertyDifference.onlyLeft){
+                this.choose(this.leftSideMap.get( key ));
+            }
+        }
+
+        for ( let key of Array.from( this.rightSideMap.keys() ) ) {
+            let propertyDifference = new PropertyDifference( key, this.leftSideMap.get( key ), this.rightSideMap.get( key ) );
+            this.differencesMap.set( key, propertyDifference );
+            if(propertyDifference.equal || propertyDifference.onlyRight){
+                this.choose(this.rightSideMap.get( key ));
+            }
         }
     }
-    
-    loadRightSideProperties() {
-        for ( let i = 0; i < 5; i++ ) {
-            var property = new Property( i, "property_" + i, "value_" + i );
-            this.rightSideProperties[i] = property;
+
+    getKeys( map ) {
+        return Array.from( map.keys() );
+    }
+
+    choose( prop: Property ): void {
+        if ( this.isChoosed( prop ) ) {
+            this.differencesMap.get( prop.name ).choosed = null;
+        } else {
+            this.differencesMap.get( prop.name ).choosed = prop;
         }
+    }
+
+    isChoosed( prop: Property ): boolean {
+        return prop == this.differencesMap.get( prop.name ).choosed;
     }
 }
---
+++ Software/src/app/mergeFiles/property.difference.model.ts
@@ -0,0 +1,37 @@
+import { Property } from "./property.model";
+
+export class PropertyDifference {
+
+    public equal: boolean = false;
+    public onlyLeft: boolean = false;
+    public onlyRight: boolean = false;
+    public choosed: Property;
+
+    constructor(
+        public name: string,
+        public leftProperty?: Property,
+        public rightProperty?: Property
+    ) {
+        
+        
+        
+        if ( leftProperty == null && rightProperty == null ) {
+            throw new Error("Can not compare empty properties");
+        }
+
+        this.onlyRight = ( leftProperty == null && rightProperty != null );
+        this.onlyLeft = ( leftProperty != null && rightProperty == null );
+        
+        if ( leftProperty != null && rightProperty != null ) {
+            this.equal = this.compare( leftProperty, rightProperty );
+        }
+        
+        if(leftProperty != null && name !== leftProperty.name || rightProperty != null && name !== rightProperty.name){
+            throw new Error("Can not compare properties with different names");
+        }
+    }
+
+    private compare( leftProperty: Property, rightProperty: Property ): boolean {
+        return ( leftProperty.value === rightProperty.value );
+    }
+}
